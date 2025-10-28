c51e48866355e71bd6779af2e8d482414d00cb7f
Cameron Jones <camandrewjones@gmail.com>
2025-10-28 16:50:45 +0200

    remove tanstack dev tools on prod

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -40,17 +40,19 @@ function RootDocument({ children }: { children: React.ReactNode }) {
       <body>
         <Header />
         {children}
-        <TanStackDevtools
-          config={{
-            position: 'bottom-right',
-          }}
-          plugins={[
-            {
-              name: 'Tanstack Router',
-              render: <TanStackRouterDevtoolsPanel />,
-            },
-          ]}
-        />
+        {import.meta.env.DEV && (
+          <TanStackDevtools
+            config={{
+              position: 'bottom-right',
+            }}
+            plugins={[
+              {
+                name: 'Tanstack Router',
+                render: <TanStackRouterDevtoolsPanel />,
+              },
+            ]}
+          />
+        )}
         <Scripts />
       </body>
     </html>
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,14 +1,8 @@
 import { ServiceBubble } from '@/components/ServiceBubble'
 import { createFileRoute } from '@tanstack/react-router'
 import {
-  Zap,
   Droplets,
   Truck,
-  Server,
-  Route as RouteIcon,
-  Shield,
-  Waves,
-  Sparkles,
 } from 'lucide-react'
 
 export const Route = createFileRoute('/')({ component: App })
